2635fd172b772ec740a0e05c6e877dbe811208db
Ahmet <ahmetbostancikli@gmail.com>
2022-07-19 01:44:42 +0300

    Initial Commit

---
+++ Calculator/Model/Constant.swift
@@ -0,0 +1,37 @@
+//
+//  Constant.swift
+//  Calculator
+//
+//  Created by Ahmet Bostancıklıoğlu on 19.07.2022.
+//
+
+import Foundation
+
+
+struct numberGroup {
+    static let ac = "AC"
+    static let sign = "+/-"
+    static let percent = "%"
+    static let positive = "+"
+    static let negative = "-"
+    static let dot = "."
+    
+    static let zero = "0"
+    static let one = "1"
+    static let two = "2"
+    static let three = "3"
+    static let four = "4"
+    static let five = "5"
+    static let six = "6"
+    static let seven = "7"
+    static let eight = "8"
+    static let nine = "9"
+}
+
+struct operatorGroup {
+    static let add = "+"
+    static let sub = "-"
+    static let multiply = "x"
+    static let divide = "÷"
+    static let result = "="
+}
---
+++ Calculator/View Controller/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ahmet Bostancıklıoğlu on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //MARK: - IBOutlets
+    
+//    Results label
+    @IBOutlet weak var resultLabel: UILabel!
+    
+//    function button
+    @IBOutlet weak var ACButton: UIButton!
+    @IBOutlet weak var posNegButton: UIButton!
+    @IBOutlet weak var modulatButton: UIButton!
+    
+//    operation button
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var addbutton: UIButton!
+    @IBOutlet weak var resultButton: UIButton!
+    
+//    number button
+    @IBOutlet weak var buttonDot: UIButton!
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
+    
+   var dataManager = DataManager()
+    
+    //MARK: - ViewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        dataManager.delegate = self
+
+    }
+
+    //MARK: - make buttons round
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let buttons = [
+                       ACButton , posNegButton , modulatButton , divideButton ,
+                       multiplyButton , subtractButton , addbutton , resultButton , buttonDot ,
+                       button1 , button2 , button3 , button4 , button5 , button6 ,
+                       button7 , button8 , button9
+        ]
+        
+        //make buttons round
+        
+        for button in buttons {
+            if let btn = button {
+                btn.layer.cornerRadius = btn.bounds.width / 2
+            }
+            button0.layer.cornerRadius = button0.bounds.height / 2
+        }
+    }
+}
+
+
+//MARK: - View Actions
+
+extension ViewController: DataManagerDelegate {
+    
+    //MARK: - IBActions
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        dataManager.proccessNumber(button: sender, labelText: resultLabel.text!)
+    }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        dataManager.proccessOperator(button: sender)
+    }
+    
+    @IBAction func resultPressed(_ sender: UIButton) {
+        dataManager.getResult()
+    }
+    
+    //MARK: - DataManagerDelegate stubs
+    
+    func updateView(result: String) {
+        resultLabel.text = result
+    }
+
+}
